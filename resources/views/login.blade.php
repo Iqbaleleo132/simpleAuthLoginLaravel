5e4c1b2152e2a93cc9ec5c88fad2230a5f90cfec
Iqbaleleo132 <maulanaeiqbalee@gmail.com>
2023-11-29 01:56:06 +0700

    update dikit

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -13,28 +13,52 @@
             <li>{{$item}}</li>
         @endforeach
     </div>
-        
     @endif
 </head>
 <body>
-    <div class="container py-5">
-        <div class="w-50 center border rounded px-3 py-3 mx-auto">
-        <h1>Login</h1>
+
+
+    <div class="container my-auto w-50 overflow-visible ">
+        <div class="bg-white rounded-1 shadow border border-1  ">
+            <div class="container-fluid py-3">
+                <p class="m-0 px-3 py-1 ">
+                    <span class="text-secondary fs-2">
+                        Welcome back!
+                    </span>
+                </p>
+            </div>
+            <div class="container-fluid">
+                <p class="text-dark fs-5 fw-bold font-family-Mulish  m-0 px-3 py-2">
+                    Sign in to your account
+                </p>
+
+                @foreach ($errors->all() as $item)
+                <div class="alert alert-primary" role="alert">
+                    SILAHKAN DIULANG LAGI
+                </div>
+                    
+                @endforeach
+            </div>
         <form action="" method="POST">
             @csrf
-            <div class="mb-3">
-                <label for="username" class="form-label">Username</label>
-                <input type="text" value="{{old('nama')}}" name="nama" class="form-control">
+            <div class="px-4 mb-4">
+                <label for="" class="form-label">Username</label>
+                <input class="form-control" type="text" value="{{ old('nama') }}" name="nama">
             </div>
-            <div class="mb-3">
-                <label for="password" class="form-label">Password</label>
-                <input type="password" name="password" class="form-control">
+            <div class="px-4 mb-4">
+                <label for="" class="form-label">Password</label>
+                <input class="form-control" type="password" name="password">
             </div>
-            <div class="mb-3 d-grid">
+            <div class="px-4 mb-4">
                 <button name="submit" type="submit" class="btn btn-primary">Login</button>
             </div>
-        </form>
-    </div> 
+            </form>
+        </div>
     </div>
+
+   
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
 </body>
 </html>
